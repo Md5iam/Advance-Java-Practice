10a5f57f59baf1046bf4881ffd7b69d975b904b5
md5iam <mdsiam9837@gmail.com>
2026-08-06 22:14:16 +0600

    remove folder

--- ProductShop/src/main/java/org/example/productshop/ProductService.java
+++
@@ -1,8 +0,0 @@
-package org.example.productshop;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductService {
-
-}
--- demo/src/main/java/bd/edu/seu/nookmanagementsystem/Launcher.java
+++
@@ -1,7 +0,0 @@
-package bd.edu.seu.nookmanagementsystem;
-
-public class Launcher {
-    public static void main(String[] args) {
-        HelloApplication.main(args);
-    }
-}
